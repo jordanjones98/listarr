9e46322a37f3cc6f8ad173e388ca4f134c65bfd9
Jordan Jones <me@jjones.tech>
2025-12-01 20:14:18 -0600

    add admin middleware

---
+++ src/middleware.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const authHeader = request.headers.get("authorization");
+
+  if (authHeader) {
+    const authValue = authHeader.split(" ")[1];
+    const [user, pwd] = atob(authValue).split(":");
+
+    const validUser = process.env.ADMIN_USERNAME || "admin";
+    const validPwd = process.env.ADMIN_PASSWORD || "admin";
+
+    if (user === validUser && pwd === validPwd) {
+      return NextResponse.next();
+    }
+  }
+
+  return new NextResponse("Authentication required", {
+    status: 401,
+    headers: {
+      "WWW-Authenticate": 'Basic realm="Admin Area"',
+    },
+  });
+}
+
+export const config = {
+  matcher: "/admin/:path*",
+};
